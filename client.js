e7e46c0004692d4e05204b41ee59ad8d1922eaf7
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-15 09:32:09 -0600

    turns answer black, next step is IF ELSE to only choose wrong answer black, later I will try to change each clicked color to a unique color

--- client.js
+++ client.js
@@ -1,48 +1,23 @@
 $(document).ready(function(){
-  console.log('hi');
-  var notAButton = $('<div id="notaButton">'); //changed to just a div
-  notAButton.text('Click on a color until you find the one I\'m thinking of.  It will change each time you refresh the page');
-  $('#buttonPlace').append(notAButton);
-
-    var redColorBox = $('<div data-number="0" class="redColorBox"></div>');
-    var blueColorBox = $('<div data-number="1" class="blueColorBox"></div>');
-    var yellowColorBox = $('<div data-number="2" class="yellowColorBox"></div>');
-    var greenColorBox = $('<div data-number="3" class="greenColorBox"></div>');
-
-    $('body').append(redColorBox, blueColorBox, yellowColorBox, greenColorBox);
-    var randomBlock = randomNumber(0,3);
-
-    // $('div').on('click', function(){  THIS WORKS BUT GOING TO PUT IT IN IF ELSE
-    //     console.log($(this).data());
-    //     console.log($(this).data().number == randomBlock);
-    //     $('#colorPlace').text("Your guess is ... bum bum bum..." + ($(this).data().number == randomBlock));
-    //   });
-
-      $('div').on('click', function(){
-          console.log($(this).data());
-          console.log($(this).data().number == randomBlock);
-          $('#colorPlace').text("Your guess is ... bum bum bum..." + ($(this).data().number == randomBlock));
-        });
-
-
-
-
-    function randomNumber(min, max){
-        return Math.floor(Math.random() * (1 + max - min) + min);
-    }
-  //});
-
-  // for (var i = 0; i < 10; i++) {
-  //   var newButton = $('<button>'); //creating it so include angle brackets - jquery doesnt need closing brackets
-  //   newButton.text('click me I am a button');
-  //   newButton.data('idNumber', i); // idNumber is 1, changing to i - ids are common but often not displayed on page- .data stores
-  //   newButton.data('cool secret', 'Kris has big ears'); // first is key, second is value
-  //   newButton.data('cool secret', 'other cool secret'); // this one overrides the first
-  //   $('body').append(newButton);
-  // }
-  //
-
-
-  // console.log($(this).data());
-  // console.log($(this).data().idNumber + 'button was clicked!'); //idNumber is value?
+  var redColorBox = $('<div data-number="0" class="redColorBox"></div>');
+  var blueColorBox = $('<div data-number="1" class="blueColorBox"></div>');
+  var yellowColorBox = $('<div data-number="2" class="yellowColorBox"></div>');
+  var greenColorBox = $('<div data-number="3" class="greenColorBox"></div>');
+  var myColorArray = [redColorBox, blueColorBox, yellowColorBox, greenColorBox]
+  $('body').append(myColorArray);
+//above assigns a jquery object div to a var - below
+  var randomBlock = randomNumber(0,3);
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  }
+  $('div').on('click', function(){
+      $('#colorPlace').text("Your guess is ... bum bum bum..." + ($(this).data().number == randomBlock));
+      $(this).css('background-color', 'black');
+  });
 });
+
+// THIS WORKS - (but turns all clicked divs black - NOW GOING TO PUT IT IN IF ELSE
+// $('div').on('click', function(){
+//     $('#colorPlace').text("Your guess is ... bum bum bum..." + ($(this).data().number == randomBlock));
+//     $(this).css('background-color', 'black');
+// });
